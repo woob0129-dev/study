dbd2709f1f31c0ba4e516518ecb6d97153841082
hdw <dev.h.dio@gmail.com>
2026-03-26 22:32:09 +0900

    코테 세팅

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,15 @@
+import codetest.backjoon.*
+import codetest.programmers.*
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 fun main() {
-    val name = "Kotlin"
-    //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-    // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-    println("Hello, " + name + "!")
+    val input = readln()
+    println("input $input")
 
-    for (i in 1..5) {
-        //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-        // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-        println("i = $i")
-    }
+    val input2 = readln()
+    println("input2 $input2")
+
+    val answer = solution12901(input.toInt(), input2.toInt())
+    println("answer $answer")
 }
